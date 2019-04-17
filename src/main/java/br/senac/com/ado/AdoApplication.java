b066ff0e42f42653cf48875f86daf2e31ccba776
Eduardo Felipe Donadio de Almeida Serra <efelipe10@hotmail.com>
2019-04-17 09:26:41 -0300

    - Criação do projeto

---
+++ src/main/java/br/senac/com/ado/AdoApplication.java
@@ -0,0 +1,13 @@
+package br.senac.com.ado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoApplication.class, args);
+    }
+
+}
